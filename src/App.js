c07fb4719007f6d540e2b6f2412c4f279ef5d581
Pavlo <Pmoroz2004@gmail.com>
2022-07-03 13:31:13 +0300

    build

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
+// const API_URL = 'https://production.api';
+const API_URL = 'https://development.api';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {API_URL}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {process.env.NODE_ENV}
       </header>
     </div>
   );
